eba920307f479b24f7a6d383d8119fd24c6a5245
HalierC <115671323+haliercesr@users.noreply.github.com>
2023-12-20 11:06:20 -0300

    Update configURL.js

--- src/configURL.js
+++ src/configURL.js
@@ -1,4 +1,4 @@
 //const URLfrontend="http://localhost:3000"
-const URLfrontend="https://dogs-page.onrender.com";
+const URLfrontend="https://pidogs2023-1tj3ptx16-haliercesr.vercel.app/";
 
 module.exports=URLfrontend
