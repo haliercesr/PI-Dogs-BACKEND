9d8ff88a958adb184c37c38847ad3ba21bb67f71
HalierC <115671323+haliercesr@users.noreply.github.com>
2023-12-20 11:11:46 -0300

    Update configURL.js

--- src/configURL.js
+++ src/configURL.js
@@ -1,4 +1,4 @@
 //const URLfrontend="http://localhost:3000"
-const URLfrontend="https://pidogs2023-1tj3ptx16-haliercesr.vercel.app/";
+const URLfrontend="https://pidogs2023.vercel.app/";
 
 module.exports=URLfrontend
