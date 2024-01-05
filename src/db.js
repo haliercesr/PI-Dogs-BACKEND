2175989d2a94cfb8a41b2876af1a7c94cd84906a
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-01-05 14:46:16 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-   dialectModule: require('pg')
+   dialectModule: require('pg'),
 });
 const basename = path.basename(__filename);
 
