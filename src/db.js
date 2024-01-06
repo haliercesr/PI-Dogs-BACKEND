43bd92482578fc7ebabe3f1a1c2ba8ecf2b3c80d
HalierC <115671323+haliercesr@users.noreply.github.com>
2024-01-06 20:10:23 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,DATABASE,DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectModule: pg,
